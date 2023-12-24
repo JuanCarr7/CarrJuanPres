bf885075aa4448893edd2cd50c0f10be87ca5061
114054-Carreggio-JuanCruz <Carreggio22@gmail.com>
2023-12-24 18:37:49 -0300

    pagina de amigo invisible proptotipo

--- carrJuanPres/src/App.jsx
+++ carrJuanPres/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
           <h1 className="text-neutral-50"> < a className="underline decoration-sky-500">Hola lore, como estas?</a> soy tu  <a className="text-neutral-50/25">amigo invisible</a> este año. Espero me descubras y te guste el regalo🎁, cualquier cosa se puede cambiar en azalea jaja!</h1>
         </div>
         <div  className="flex justify-center items-center mt-10">
-          <h1>Aqui te dejo mis pistas:</h1>
+          <h1>Aqui te dejo mis pistas, algunas son mentira:</h1>
         </div>
         <Pistas/>
         <Footer/>
